b9dbcfa6a52b1c5329c98cf1fba814ab27827a15
植草 智輝 <tmkueks19@gmail.com>
2015-02-04 22:23:02 +0900

    security soltの設定

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,7 +222,7 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'CShg93b0qsJfIafsbgrVodubhwvviR2G0FgaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
