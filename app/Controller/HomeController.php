8881faa7600d7fecaa1de09da9089e56285cdb09
植草 智輝 <tmkueks19@gmail.com>
2015-02-06 02:34:40 +0900

    jsonから配列にして、ビューに渡すように変更、その他レイアウト調整

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -1,15 +1,17 @@
 <?php
 set_include_path(APP.'vendors/');
+App::uses('Xml', 'Utility');
 
 class HomeController extends AppController {
    	public $helpers = array('Html', 'Form' );
 	public function index(){
 		// API のパラメータ設定
-		$api = 'http://shopping.yahooapis.jp/ShoppingWebService/V1/itemSearch';
+		$api = 'http://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch';
 		$appid = 'dj0zaiZpPThma1IzcVpTRThzViZzPWNvbnN1bWVyc2VjcmV0Jng9YzY-';
 
 		$params = array(
-			'query' => 'vaio'
+			'query' => 'vaio',
+			'hits' => 50
 		);
 		// GETクエリの生成
 		$ch = curl_init($api.'?'.http_build_query($params));
@@ -18,10 +20,19 @@ class HomeController extends AppController {
 		    CURLOPT_USERAGENT      => "Yahoo AppID: $appid"
 		));
 		
+		// GETの実行
 		$result = curl_exec($ch);
+		// セッションの終了
 		curl_close($ch);
-		$this -> set('test_request', "this is test request");
-		$this -> set('api_result', $result);
+		
+		// XMLの文字列を配列に変換
+		//$xml_array = Xml::toArray(Xml::build($result));
+		
+		// jsonの文字列を配列に変換
+		$json_array = json_decode($result,true);
+		
+		$this -> set('query', "vaio");
+		$this -> set('api_result', $json_array);
 	}
 }
 ?>
