d58ff8213c15ab322fcaab43db539f03a6f41d45
植草 智輝 <tmkueks19@gmail.com>
2015-02-05 21:12:13 +0900

    商品検索apiのテスト

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -1,8 +1,27 @@
 <?php
+set_include_path(APP.'vendors/');
+
 class HomeController extends AppController {
    	public $helpers = array('Html', 'Form' );
 	public function index(){
+		// API のパラメータ設定
+		$api = 'http://shopping.yahooapis.jp/ShoppingWebService/V1/itemSearch';
+		$appid = 'dj0zaiZpPThma1IzcVpTRThzViZzPWNvbnN1bWVyc2VjcmV0Jng9YzY-';
+
+		$params = array(
+			'query' => 'vaio'
+		);
+		// GETクエリの生成
+		$ch = curl_init($api.'?'.http_build_query($params));
+		curl_setopt_array($ch, array(
+		    CURLOPT_RETURNTRANSFER => true,
+		    CURLOPT_USERAGENT      => "Yahoo AppID: $appid"
+		));
+		
+		$result = curl_exec($ch);
+		curl_close($ch);
 		$this -> set('test_request', "this is test request");
+		$this -> set('api_result', $result);
 	}
 }
 ?>
