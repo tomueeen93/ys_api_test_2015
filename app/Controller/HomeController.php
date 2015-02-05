120b2aceca5e2af7ddc87ae05664ac6209a3dd5d
植草 智輝 <tmkueks19@gmail.com>
2015-02-05 21:12:13 +0900

    商品検索apiの利用テスト

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -1,8 +1,32 @@
 <?php
+set_include_path(APP.'vendors/');
+
 class HomeController extends AppController {
    	public $helpers = array('Html', 'Form' );
 	public function index(){
+		// API のパラメータ設定
+		//$api = 'http://jlp.yahooapis.jp/FuriganaService/V1/furigana';
+		$api = 'http://shopping.yahooapis.jp/ShoppingWebService/V1/itemSearch';
+		$appid = 'dj0zaiZpPThma1IzcVpTRThzViZzPWNvbnN1bWVyc2VjcmV0Jng9YzY-';
+		/*
+		$params = array(
+		    'sentence' => '明鏡止水'
+		);
+		*/
+		$params = array(
+			'query' => 'vaio'
+		);
+		// GETクエリの生成
+		$ch = curl_init($api.'?'.http_build_query($params));
+		curl_setopt_array($ch, array(
+		    CURLOPT_RETURNTRANSFER => true,
+		    CURLOPT_USERAGENT      => "Yahoo AppID: $appid"
+		));
+		
+		$result = curl_exec($ch);
+		curl_close($ch);
 		$this -> set('test_request', "this is test request");
+		$this -> set('api_result', $result);
 	}
 }
 ?>
--- app/View/Home/index.ctp
+++ app/View/Home/index.ctp
@@ -1,2 +1,7 @@
 <h1>API test page</h1>
-<?php echo $test_request ?>
+<ul>
+	<li><?php echo $test_request ?></li>
+	<?php foreach ((array)$api_result as $each): ?>
+		<li><?php echo $each ?></li>
+	<?php endforeach; ?>
+</ul>
